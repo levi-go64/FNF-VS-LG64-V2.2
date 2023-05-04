717f69947d01fce50031d0fd91ea6f170fec4d98
newbion10 <96935020+newbion10@users.noreply.github.com>
2023-05-04 19:31:31 +0900

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2244,7 +2244,7 @@ class PlayState extends MusicBeatState
 	public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Misses: ' + songMisses
+		+ ' | Port by newbion | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
