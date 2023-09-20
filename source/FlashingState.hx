f6419d177eac5c9579add5cd412283df8f6163d2
LG64 <96037133+levi-go64@users.noreply.github.com>
2023-09-20 07:56:14 +0700

    Testing again

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,11 +24,11 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
+			"Hey, uh thanks for playing this mod, before that watch out guys!\n
+			This Mod sometimes contains some flashing lights!\n
+			Press ENTER or A to disable them now or go to Options Menu.\n
+			Press ESCAPE Or B to ignore this message.\n
+			Have Fun Greeting From LG64!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
